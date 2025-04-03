f51ba7d9282fcb6a34b358907487e904ebcfefa2
Sean_codez <sdillon215@gmail.com>
2025-04-03 14:37:23 -0700

    vercel deploy fix commented out newsletter files/imports to fix missing api key error

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -1,23 +1,23 @@
-import { Resend } from "resend";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const POST = async (request: Request) => {
-  const { email } = await request.json();
+// export const POST = async (request: Request) => {
+//   const { email } = await request.json();
 
-  // Create contact
-  try {
-    resend.contacts.create({
-      email,
-      unsubscribed: false,
-      audienceId: process.env.RESEND_AUDIENCE_ID!,
-    });
+//   // Create contact
+//   try {
+//     resend.contacts.create({
+//       email,
+//       unsubscribed: false,
+//       audienceId: process.env.RESEND_AUDIENCE_ID!,
+//     });
 
-    return Response.json({ success: true });
-  } catch (error: any) {
-    return Response.json(
-      { error: "Error subscribing to updates" },
-      { status: 400 }
-    );
-  }
-};
+//     return Response.json({ success: true });
+//   } catch (error: any) {
+//     return Response.json(
+//       { error: "Error subscribing to updates" },
+//       { status: 400 }
+//     );
+//   }
+// };
